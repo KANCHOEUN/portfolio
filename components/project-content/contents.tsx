a80c4cf0c19a86c42e6c0295364ede41be256552
KANCHOEUN <kancho1216@naver.com>
2025-09-30 15:54:17 +0000

    feat: Pigrest 설계 및 구현 내용 추가

--- components/project-content/contents.tsx
+++ components/project-content/contents.tsx
@@ -42,21 +42,22 @@ export function Project1DiagramContent() {
           className="text-[#61afef] dark:text-[#61afef] light:text-[#4078f2] hover:text-[#56b6c2] dark:hover:text-[#56b6c2] light:hover:text-[#0184bc] transition-colors underline mb-2"
         >
           블로그 글에서 자세히 보기
-        </button>
+        </button><br/><br/>
+        ※ 게시물 기능은 사용자가 작성중인 게시물을 임시로 저장하는 <b>초안 단계</b>와 실제로 등록하는 <b>발행 단계</b>로 나뉩니다.
         <ImageZoom src="/images/posts/pigrest/auto-save-sequence.svg" alt="auto-save-sequence" className="my-4 rounded"
         caption="Publish한 적 없는 게시글에 대한 자동 저장 시스템의 시퀀스 다이어그램"/>
         <br/>
         <br/>
         <div>
-        사용자 입력 시 발생하는 DB 부하를 줄이기 위해 <b>Write Behind</b> 전략을 적용한 자동 저장 시스템을 구현하였습니다.
+        <b>사용자 입력</b> 시 발생하는 DB 부하를 줄이기 위해 <b>Write Behind</b> 전략을 적용한 자동 저장 시스템을 구현하였습니다.
         <br/>
         입력한 데이터는 <b>Redis Hash에 임시 저장(draft)</b>되며, Spring Scheduler를 통해 <b>주기적으로 DB에 비동기 반영</b>됩니다.
         초기 `draft:*` 패턴으로 전체 key를 스캔하는 방식에서 <b>최신 업데이트된 데이터의 key를 관리하는 Set</b> 기반 방식으로 개선하여,
-        <b> 중복 동기화를 방지</b>하고 시간 복잡도를 O(N)에서 <b>O(1)로 최적화</b>했습니다. 
+        <b> 중복 동기화를 방지</b>하고 시간 복잡도를 <b>O(N)에서 O(1)로 최적화</b>했습니다. 또한 <b>Redis 파이프라인</b>을 적용하여 <b>IO 성능을 개선</b>함으로써 1000개의 요청을 기준으로 <b>약 1450ms에서 264ms로 단축</b>했습니다.
         <br/>
         <b>Cache Aside</b> 전략을 통해 실시간 데이터 조회와 다중 기기 간 작업 연속성을 보장했으며, <b>TTL을 활용</b>하여 임시 데이터의 생명 주기를 관리했습니다.
-        <br/>
-        게시물 발행(publish) 시 Redis에 저장된 데이터를 즉시 삭제하여 메모리 효율성을 높였습니다.
+        <br/><br/>
+        <b>게시물 발행</b>(publish) 시 <b>Unique Constraint</b>를 적용하여 <b>중복 요청을 방지</b>하여 멱등성을 보장했습니다. 그와 동시에 Redis에 저장된 데이터를 즉시 삭제하여 메모리 효율성 또한 높였습니다.
         </div>
       </section>
       {/* <section id="erd" className="mb-6">
